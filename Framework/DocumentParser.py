8c6bb17bedefaa898490400fd6192fe53c24a7ff
Daniel <d.alnemri@mail.utoronto.ca>
2023-10-06 13:59:42 -0400

    Add bookmark ignoring from document

--- Framework/DocumentParser.py
+++ Framework/DocumentParser.py
@@ -28,7 +28,7 @@ class PDFParser(DocumentParserBase):
     Examples:
         Download the PDF from https://zenodo.org/record/50395 to give it a try
     """
-    def breakdown_document(document, reader: PdfReader = None, max_tokens: int = 1000, only_alphaNumeric: bool = False) -> Dict[Union[str, int], str]:
+    def breakdown_document(document, reader: PdfReader = None, max_tokens: int = 1000, only_alphaNumeric: bool = False, bookmarks_to_ignore: set = set()) -> Dict[Union[str, int], str]:
         if isinstance(document, str):
             pdfReader = PdfReader(document).outline
             reader = PdfReader(document)
@@ -41,45 +41,54 @@ class PDFParser(DocumentParserBase):
             item = bookmarks[i]
             if isinstance(item, list):
                 #recursive call
-                result.update(PDFParser.breakdown_document(item, reader))
+                result.update(PDFParser.breakdown_document(document=item, reader=reader, bookmarks_to_ignore=bookmarks_to_ignore))
             else:
                 page_index = reader.get_destination_page_number(item)
                 bookmark_name = item.title
-                    
-                # Get the page number of the next bookmark
-                if i + 1 < len(bookmarks):
-                    next_item = bookmarks[i + 1]
-                    if isinstance(next_item, list):
-                        if next_item:
-                            next_page_index = reader.get_destination_page_number(next_item[0])
+                if(bookmark_name.strip().lower() not in {bookmark.lower() for bookmark in bookmarks_to_ignore}): 
+                    # Get the page number of the next bookmark
+                    if i + 1 < len(bookmarks):
+                        next_item = bookmarks[i + 1]
+                        if isinstance(next_item, list):
+                            if next_item:
+                                next_page_index = reader.get_destination_page_number(next_item[0])
+                            else:
+                                continue
                         else:
-                            continue
+                            next_page_index = reader.get_destination_page_number(next_item)
                     else:
-                        next_page_index = reader.get_destination_page_number(next_item)
-                else:
-                    # If this is the last bookmark, get the number of the last page in the PDF
-                    next_page_index = len(reader.pages)
+                        # If this is the last bookmark, get the number of the last page in the PDF
+                        next_page_index = len(reader.pages)
 
-                # Extract all pages from the current bookmark up to (but not including) the next bookmark
-                bookmark_content = ""
-                for page in reader.pages[page_index:next_page_index]:
-                    page_text = page.extract_text()
-                    # Remove non-alphabetic characters
-                    if only_alphaNumeric:
-                        page_text = re.sub(r'\W+', ' ', page_text)
-                    bookmark_content += page_text
+                    # Extract all pages from the current bookmark up to (but not including) the next bookmark
+                    bookmark_content = ""
+                    for page in reader.pages[page_index:next_page_index]:
+                        page_text = page.extract_text()
+                        # Remove non-alphabetic characters
+                        if only_alphaNumeric:
+                            page_text = re.sub(r'\W+', ' ', page_text)
+                        bookmark_content += page_text
 
-                # Split the content into chunks of max_tokens
-                tokens = bookmark_content.split()
-                for j in range(0, len(tokens), max_tokens):
-                    chunk = " ".join(tokens[j:j+max_tokens])
-                    result[f"{bookmark_name}_{j//max_tokens}"] = chunk          
+                    # Split the content into chunks of max_tokens
+                    tokens = bookmark_content.split()
+                    for j in range(0, len(tokens), max_tokens):
+                        chunk = " ".join(tokens[j:j+max_tokens])
+                        print('Adding: ' + f"{bookmark_name}_{j//max_tokens}")
+                        result[f"{bookmark_name}_{j//max_tokens}"] = chunk    
 
         return result
 
-'''Example Usage
-bms =  PDFParser.breakdown_document("benefits-booklet.pdf", max_tokens=4000, only_alphaNumeric=False)
-print(bms)
+'''Example Usage'''
+'''bms =  PDFParser.breakdown_document("Rosen, Kenneth H - Discrete mathematics 8th ed.pdf", 
+                                    max_tokens=4000, only_alphaNumeric=False, 
+                                    bookmarks_to_ignore={'Answers to Odd-Numbered Exercises', 'End-of-Chapter Material'})
+
+csv_file = "MathTextbook.csv"
+import pandas as pd
+
+(pd.DataFrame.from_dict(data=bms, orient='index')
+   .to_csv(csv_file, header=False))'''
+'''print(bms)
 
 for bm in bms:
     print(bm)
