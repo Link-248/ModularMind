42ad2a6695c527b9274aad0e93457c9643238c4e
Daniel <d.alnemri@mail.utoronto.ca>
2023-10-10 21:09:31 -0400

    Add embeddings for unlimited column csv
    -other bug fixes

--- framework/knowledge/Embeddings.py
+++ framework/knowledge/Embeddings.py
@@ -1,11 +1,8 @@
-from typing import Dict, Union
 import openai
-from abc import ABC, abstractmethod
-import tiktoken
 import pandas as pd
-import json
+import chardet
 
-class Embeddings():
+class TextEmbeddings():
     """
     OpenAI Embeddings model, convert multiple data set types to multiple embedding data set types
     """
@@ -29,8 +26,8 @@ class Embeddings():
     def get_embedding(self, text, model: str ='text-embedding-ada-002'):
         response = openai.Embedding.create(input = text, model=model)
         return response['data'][0]['embedding']
-    
-    def csv_to_embeds_list(self, data_path: str, model: str ='text-embedding-ada-002'):
+        
+    def two_header_csv_to_embeds_list(self, data_path: str, model: str ='text-embedding-ada-002'):
         # load & inspect dataset
         input_datapath = data_path  # to save space, we provide a pre-filtered dataset
         df = pd.read_csv(input_datapath, header=None)
@@ -52,7 +49,7 @@ class Embeddings():
         
         return embeds
     
-    def csv_to_embeds_dict(self, data_path: str, model: str ='text-embedding-ada-002'):
+    def two_header_csv_to_embeds_dict(self, data_path: str, model: str ='text-embedding-ada-002'):
         # load & inspect dataset
         input_datapath = data_path  # to save space, we provide a pre-filtered dataset
         df = pd.read_csv(input_datapath, header=None)
@@ -75,7 +72,7 @@ class Embeddings():
         
         return embeds
     
-    def csv_to_embeds_csv(self, data_path: str, model: str ='text-embedding-ada-002'):
+    def two_header_csv_to_embeds_csv(self, data_path: str, output_path: str, model: str ='text-embedding-ada-002'):
         # load & inspect dataset
         input_datapath = data_path  # to save space, we provide a pre-filtered dataset
         df = pd.read_csv(input_datapath, header=None)
@@ -91,9 +88,9 @@ class Embeddings():
         df_embeddings = df_embeddings.join(df['content'].apply(lambda x: list(self.get_embedding(x, model=model))))
         
         # Save the DataFrame to a CSV file
-        df_embeddings.to_csv('embedded_RAP.csv', index=False)
+        df_embeddings.to_csv(output_path, index=False)
     
-    def dict_to_embeds_csv(self, data_dict: dict, model: str ='text-embedding-ada-002'):
+    def dict_to_embeds_csv(self, data_dict: dict, output_path: str, model: str ='text-embedding-ada-002'):
         df = pd.DataFrame(list(data_dict.items()), columns=['topic', 'content'])
         
         # Create a new DataFrame with 'topic' as the first column
@@ -106,7 +103,7 @@ class Embeddings():
         df_embeddings = df_embeddings.join(df['content'].apply(lambda x: list(self.get_embedding(x, model=model))))
         
         # Save the DataFrame to a CSV file
-        df_embeddings.to_csv('embedded_RAP2.csv', index=False)
+        df_embeddings.to_csv(output_path, index=False)
         
     def dict_to_embeds_list(self, data_dict: dict, model: str ='text-embedding-ada-002'):
         df = pd.DataFrame(list(data_dict.items()), columns=['topic', 'content'])
@@ -145,9 +142,44 @@ class Embeddings():
         for i in range(df_embeddings.index.size):
             embeds[topics[i]] = (set(str(df_embeddings.loc[i,'content']).strip('[').strip(']').split(', ')))
         return embeds
+    
+    def csv_to_embeds_csv(self, data_path: str, output_path: str, model: str ='text-embedding-ada-002'):
+        # Read the file with the detected encoding
+        try:
+            # Try common file encodings
+            encoding = self.try_encodings(data_path)
+            df = pd.read_csv(data_path, encoding=encoding,  header=None)
+        except:
+            # Detect the file's encoding
+            with open(data_path, 'rb') as f:
+                result = chardet.detect(f.read())
+            df = pd.read_csv(data_path, encoding=result['encoding'], header=None)
+
+        # Create a new DataFrame to store the embeddings
+        df_embeddings = df.copy()
+
+        # Iterate over each column in the DataFrame
+        for column in df.columns:
+            # Handle NaN values
+            df[column] = df[column].fillna('')
+            # Embed the column's values
+            df_embeddings[column] = df[column].apply(lambda x: [self.get_embedding(str(x), model=model)])
+
+        # Save the DataFrame to a CSV file
+        df_embeddings.to_csv(output_path, index=False)
+
+        def try_encodings(data_path):
+            encodings = ['utf-8', 'iso-8859-1', 'windows-1252']  # Add more encodings if necessary
+            for enc in encodings:
+                try:
+                    df = pd.read_csv(data_path, encoding=enc)
+                    return df
+                except UnicodeDecodeError:
+                    pass
+            raise ValueError('None of the tried encodings work')
 
 
-'''from dotenv import load_dotenv
+from dotenv import load_dotenv
 load_dotenv()
 import os
 from DocumentParser import PDFParser
@@ -159,11 +191,11 @@ NOVA_API_KEY = os.getenv('NOVA_API_KEY')
 OPENAI_KEY = os.getenv('OPENAI_API_KEY')
 OPEN_AI_BASE = 'https://api.naga.ac/v1' #"https://zukijourney.xyzbot.net/v1"  #'https://api.nova-oss.com/v1' #"https://thirdparty.webraft.in/v1" # #"https://api.naga.ac/v1"
 
-bms = PDFParser.breakdown_document("RAP.pdf", 
+'''bms = PDFParser.breakdown_document("RAP.pdf", 
                                     max_tokens=4000, only_alphaNumeric=False, 
-                                    strip_bookmarks={'Reasoning via Planning (RAP)'})
-                                    
-embeddings = Embeddings(base_api_key=OPENAI_KEY,  useOpenAIBase=False)
+                                    strip_bookmarks={'Reasoning via Planning (RAP)'})'''
+'''                                    
+embeddings = TextEmbeddings(base_api_key=OPENAI_KEY,  useOpenAIBase=False)
 
-input_datapath = "RAP.csv"  
-embeddings.dict_to_embeds_list(bms, model='text-embedding-ada-002')'''
+input_datapath = "addresses.csv"  
+embeddings.csv_to_embeds_csv(data_path=input_datapath, output_path="OUTPUT.csv", model='text-embedding-ada-002')'''
